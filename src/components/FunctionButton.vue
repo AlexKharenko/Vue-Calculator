7c508c02acfd3c1d0e2ddd215a34930653682192
AlexKharenko <alexey16052002@gmail.com>
2022-07-27 19:36:19 +0300

    Component for buttons

---
+++ src/components/FunctionButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <button
+    class="btn btn-function"
+    :class="span2 ? 'span-2' : ''"
+    @click="btnFunction(btnValue)"
+  >
+    {{ btnValue }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "FunctionButton",
+  props: ["btnValue", "span2", "btnFunction"],
+};
+</script>
+
+<style scoped>
+.btn-function {
+  background-color: rgb(104, 104, 255);
+  border: 2px solid #b4beee;
+}
+.btn-function:hover {
+  background-color: rgb(132, 132, 251);
+}
+.span-2 {
+  grid-column: span 2;
+}
+</style>
---
+++ src/components/NumberButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <button class="btn btn-number" @click="btnFunction(btnValue)">
+    {{ btnValue }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "NumberButton",
+  props: ["btnValue", "btnFunction"],
+};
+</script>
+
+<style scoped>
+.btn-number {
+  background-color: rgb(214, 214, 245);
+  border: 2px solid #b4beee;
+}
+.btn-number:hover {
+  background-color: rgb(201, 201, 245);
+}
+</style>
